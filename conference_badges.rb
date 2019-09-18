522dd75dc53b491a2487434c2c76a130c4105d46
Lauren Dove <elledoveinc@gmail.com>
2019-09-18 02:18:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
    "Hello, my name is #{name}."
   end# Write your code here.
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |name|
+    new_array.push ("Hello, my name is #{name}.")
+  end
+  new_array
+end
