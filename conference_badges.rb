ffd9cf72c4fa4a64219a91624b076c40d24a56f8
Lauren Dove <elledoveinc@gmail.com>
2019-09-18 02:23:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,5 +15,8 @@ def assign_rooms(attendees)
   new_array = []
   attendees.each do |name|
     new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter +=1 
+    counter +=1
+  end
+  new_array
+end
     
